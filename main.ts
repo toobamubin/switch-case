0d417765e13a3e81bdfaf38f73268288da6b7522
toobamubin <161958310+toobamubin@users.noreply.github.com>
2024-03-27 08:58:11 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,60 @@
+#!  /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+//step 1:
+const answers = {
+numberOne : Number,
+numberTwo : Number,
+operator : String, 
+} = await inquirer.prompt([
+    {type: "number",
+     name : "numberOne",
+     message: "Enter your first number"
+    },
+    {type: "number",
+     name : "numberTwo",
+     message: "Enter your second number"
+    },
+    {type: "list",
+     name : "operator",
+     message: "Enter your operator",
+     choices: ["+", "-", "*", "/"]}
+])
+
+//step 2
+const {numberOne, numberTwo,operator} = answers;
+
+let result
+switch (operator) {
+    case "+":
+        result = numberOne + numberTwo;
+        break;
+    case "-":
+        result = numberOne - numberTwo;
+        break;
+    case "*":
+        result = numberOne * numberTwo;
+        break;
+    case "/":
+        result = numberOne / numberTwo;
+        break;
+    default:
+        console.log("Invalid operator");
+}
+console.log(chalk.yellow(`${numberOne} ${operator}${numberTwo}=${result}`));
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
